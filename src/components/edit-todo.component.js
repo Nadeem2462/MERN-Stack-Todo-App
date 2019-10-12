c9cebb6e64a3a43914020dcf6e0030afeb1c8286
Nadeem <nadeem2462@gmail.com>
2019-10-12 18:57:33 +0500

    Create todo fron end

---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class EditList extends Component{
+    render(){
+        return(
+            <div>
+                <p>Welcome to edit todo Component!</p>
+            </div>
+        )
+    }
+}
